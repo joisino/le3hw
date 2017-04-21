a625ca2416917785cb36ef297d484c1865829f22
joisino <joisinoo@gmail.com>
2017-04-21 16:13:46 +0900

    add jump prediction

--- fst/core/src/fetch.sv
+++ fst/core/src/fetch.sv
@@ -1,15 +1,26 @@
 module fetch( input logic clk,
               input logic         reset,
               input logic [15:0]  ALUres_mem,
-              input logic         jump,
+              input logic         jump_pred,
+              input logic [15:0]  jump_pred_adr,
+              input logic         jump_pred_miss,
+              input logic         jump_pred_adr_miss,
+              input logic [15:0]  pcinc_evac,
               input logic         en_pc,
               output logic [15:0] pcinc,
               output logic [15:0] pc );
 
    logic [15:0] nextpc;
-   
-   mux mux_pc( pcinc, ALUres_mem, jump, nextpc );
+
+   // mux mux_pc( pcinc, ALUres_mem, jump, nextpc );
    flopr pc_flop( clk, reset, en_pc, nextpc, pc );
    assign pcinc = pc + 16'b1;
 
+   always_comb begin
+      nextpc <= pcinc;
+      if( jump_pred ) nextpc <= jump_pred_adr;
+      else if( jump_pred_miss ) nextpc <= pcinc_evac;
+      else if( jump_pred_adr_miss ) nextpc <= ALUres_mem;
+   end
+   
 endmodule
---
+++ fst/core/src/jumppred.sv
@@ -0,0 +1,37 @@
+module jumppred( input logic clk, reset, 
+                 input logic         jump,
+                 input logic [15:0]  pcinc_id,
+                 input logic [15:0]  ALUres_mem,
+                 input logic [2:0]   jump_state,
+                 input logic [2:0]   jump_inst, 
+                 output logic        jump_pred,
+                 output logic [15:0] jump_pred_adr,
+                 output logic        jump_pred_miss,
+                 output logic        jump_pred_adr_miss,
+                 output logic [15:0] pcinc_evac,
+                 output logic        jump_pred_busy);
+
+   assign jump_pred_adr_miss = jump & ( jump_pred_adr != ALUres_mem );
+   assign jump_pred_miss = (!jump) & ( jump_state != 0 );
+   assign jump_pred = (!jump_pred_busy) & (jump_inst != 0);
+
+   always_ff @(posedge clk) begin
+      if( reset ) begin
+         jump_pred_busy <= 0;
+         pcinc_evac <= 0;
+         jump_pred_adr <= 0;
+      end else begin
+         if( jump_pred ) begin
+            pcinc_evac <= pcinc_id;
+            jump_pred_busy <= 1;
+         end
+         if( jump_pred_miss ) begin
+            jump_pred_adr <= ALUres_mem;
+         end
+         if( jump_state != 0 ) begin
+            jump_pred_busy <= 0;
+         end
+      end
+   end
+   
+endmodule
