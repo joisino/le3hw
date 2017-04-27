5be01d96c770b418308046976421ed482a4372ae
joisino <1029275675@ml.ist.i.kyoto-u.ac.jp>
2017-04-27 17:09:52 +0900

    pri main_mem

--- fst/core/src/jumppred.sv
+++ fst/core/src/jumppred.sv
@@ -11,26 +11,26 @@ module jumppred( input logic         clk, reset,
                  output logic [15:0] pcinc_evac,
                  output logic        jump_pred_busy);
 
-   logic [15:0] jump_table [63:0];
-   logic [1:0] jump_table_valid [63:0];
+   logic [15:0] jump_table [31:0];
+   logic [1:0] jump_table_valid [31:0];
    logic [1:0] pred_shift_register;
-   logic [11:0] pcinc_history;
-   logic [5:0] predicted_pcinc;
+   logic [9:0] pcinc_history;
+   logic [4:0] predicted_pcinc;
 
    assign jump_cannot_predict = jump & (!pred_shift_register[1]);
    assign jump_pred_adr_miss = ( jump & pred_shift_register[1] & ( jump_table[predicted_pcinc] != ALUres_mem ) ) | jump_cannot_predict;
    assign jump_pred_miss = (!jump) & pred_shift_register[1];
-   assign jump_pred = (!jump_pred_busy) & (jump_inst != 0) & ( jump_table_valid[ pcinc_id[5:0] ] >= 2 );
-   assign jump_pred_adr = jump_table[ pcinc_id[5:0] ];
+   assign jump_pred = (!jump_pred_busy) & (jump_inst != 0) & ( jump_table_valid[ pcinc_id[4:0] ] >= 2 );
+   assign jump_pred_adr = jump_table[ pcinc_id[4:0] ];
    assign jump_pred_busy = pred_shift_register[0]; // | pred_shift_register[1];
-   assign predicted_pcinc = pcinc_history[11:6];
+   assign predicted_pcinc = pcinc_history[9:5];
    
    integer i;
    always_ff @(posedge clk) begin
       if( reset ) begin
          pcinc_evac <= 0;
          pcinc_history <= 0;
-         for( i = 0; i < 64; i++ ) begin
+         for( i = 0; i < 32; i++ ) begin
             jump_table_valid[i] <= 0;
          end
          pred_shift_register <= 0;
@@ -48,7 +48,7 @@ module jumppred( input logic         clk, reset,
               3: jump_table_valid[ predicted_pcinc ] <= 3;
             endcase
          end else if( jump_pred_adr_miss ) begin // predict jump, and jump, but the address is not correct
-            jump_table[ pcinc_evac[7:0] ] <= ALUres_mem;
+            jump_table[ pcinc_evac[4:0] ] <= ALUres_mem;
          end else if( jump_pred_miss ) begin // predict jump, but really not jump
             case( jump_table_valid[ predicted_pcinc ] ) 
               0: jump_table_valid[ predicted_pcinc ] <= 0;
@@ -59,7 +59,7 @@ module jumppred( input logic         clk, reset,
          end else begin
             pred_shift_register <= { pred_shift_register[0], jump_pred };
          end
-         pcinc_history <= { pcinc_history[5:0], pcinc_id[5:0] };
+         pcinc_history <= { pcinc_history[4:0], pcinc_id[4:0] };
       end
    end
    
