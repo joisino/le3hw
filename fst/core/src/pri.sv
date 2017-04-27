5b7246a7a4a8337c48d181e9301ab1e93a6f1c05
joisino <joisinoo@gmail.com>
2017-04-27 13:12:40 +0900

    2port RAM

---
+++ fst/core/src/pri.sv
@@ -0,0 +1,39 @@
+module pri ( input logic [7:0] a,
+             input logic [2:0]  c,
+             output logic [2:0] q,
+             output logic       en );
+
+   logic [7:0] b;
+   logic [2:0] res;
+
+   assign b = { a , a } >> c;
+   assign q = res + c;
+   
+   // assign b = a;
+   // assign q = res;
+
+   always_comb begin
+      res <= 0;
+      en <= 1;
+      if( b[0] ) begin
+         res <= 0;
+      end else if( b[1] ) begin
+         res <= 1;
+      end else if( b[2] ) begin
+         res <= 2;
+      end else if( b[3] ) begin
+         res <= 3;
+      end else if( b[4] ) begin
+         res <= 4;
+      end else if( b[5] ) begin
+         res <= 5;
+      end else if( b[6] ) begin
+         res <= 6;
+      end else if( b[7] ) begin
+         res <= 7;
+      end else begin
+         en <= 0;
+      end
+   end
+   
+endmodule
--- fst/core/test/dmem.sv
+++ fst/core/test/dmem.sv
@@ -15,185 +15,124 @@ module dmem
      output logic [C-1:0] main_mem_ac,
      output logic [C-1:0] lock_ac );
 
-   logic [15:0] adr;
+   logic [15:0] read_adr, write_adr;
    logic [15:0] write_dat;
    logic iswrite;
    logic [1023:0] mutex;
 
+   logic [2:0] main_mem_write_request_q;
+   logic main_mem_write_request_en;
+   logic [C-1:0] main_mem_write_ac;
+   logic [2:0] main_mem_read_request_q;
+   logic main_mem_read_request_en;
+   logic [C-1:0] main_mem_read_ac;
+
+   logic [2:0] main_mem_write_q;
+   logic [2:0] main_mem_read_q;
+   logic main_mem_read_en;
+   
+   logic [2:0] cnt;
+
+   logic [7:0] enlock_ac;
+   logic [7:0] unlock_ac;
+
+   logic [7:0] lock_en_can;
+   
+   logic [2:0] enlock_q;
+   logic [2:0] unlock_q;
+   
+   logic enlock_do;
+   logic unlock_do;
+
+   pri pri_write_request( main_mem_write_request, cnt, main_mem_write_request_q, main_mem_write_request_en );
+   pri pri_read_request( main_mem_read_request, cnt, main_mem_read_request_q, main_mem_read_request_en );
+
+   pri pri_write( main_mem_write, cnt, main_mem_write_q, iswrite );
+   pri pri_read( main_mem_read, cnt, main_mem_read_q, main_mem_read_en );
+
+   pri pri_lock( lock_en_can, cnt, enlock_q, enlock_do );
+   pri pri_unlock( unlock_en, cnt, unlock_q, unlock_do );
+
    always_comb begin
-      main_mem_ac <= 0;
-      if( main_mem_write_request[0] ) begin
-         main_mem_ac <= 8'b00000001;
-      end else if( main_mem_write_request[1] ) begin
-         main_mem_ac <= 8'b00000010;
-      end else if( main_mem_write_request[2] ) begin
-         main_mem_ac <= 8'b00000100;
-      end else if( main_mem_write_request[3] ) begin
-         main_mem_ac <= 8'b00001000;
-      end else if( main_mem_write_request[4] ) begin
-         main_mem_ac <= 8'b00010000;
-      end else if( main_mem_write_request[5] ) begin
-         main_mem_ac <= 8'b00100000;
-      end else if( main_mem_write_request[6] ) begin
-         main_mem_ac <= 8'b01000000;
-      end else if( main_mem_write_request[7] ) begin
-         main_mem_ac <= 8'b10000000;
-      end else if( main_mem_read_request[0] ) begin
-         main_mem_ac <= 8'b00000001;
-      end else if( main_mem_read_request[1] ) begin
-         main_mem_ac <= 8'b00000010;
-      end else if( main_mem_read_request[2] ) begin
-         main_mem_ac <= 8'b00000100;
-      end else if( main_mem_read_request[3] ) begin
-         main_mem_ac <= 8'b00001000;
-      end else if( main_mem_read_request[4] ) begin
-         main_mem_ac <= 8'b00010000;
-      end else if( main_mem_read_request[5] ) begin
-         main_mem_ac <= 8'b00100000;
-      end else if( main_mem_read_request[6] ) begin
-         main_mem_ac <= 8'b01000000;
-      end else if( main_mem_read_request[7] ) begin
-         main_mem_ac <= 8'b10000000;
+      main_mem_write_ac <= 0;
+      if( main_mem_write_request_en ) begin
+         main_mem_write_ac <= 8'b0000_0001 << main_mem_write_request_q;
       end
    end
 
    always_comb begin
-      adr <= 0;
-      iswrite <= 0;
-      if( main_mem_write[0] ) begin
-         adr <= main_mem_write_adr[0];
-         write_dat <= main_mem_write_dat[0];
-         iswrite <= 1;
-      end else if( main_mem_write[1] ) begin
-         adr <= main_mem_write_adr[1];
-         write_dat <= main_mem_write_dat[1];
-         iswrite <= 1;
-      end else if( main_mem_write[2] ) begin
-         adr <= main_mem_write_adr[2];
-         write_dat <= main_mem_write_dat[2];
-         iswrite <= 1;
-      end else if( main_mem_write[3] ) begin
-         adr <= main_mem_write_adr[3];
-         write_dat <= main_mem_write_dat[3];
-         iswrite <= 1;
-      end else if( main_mem_write[4] ) begin
-         adr <= main_mem_write_adr[4];
-         write_dat <= main_mem_write_dat[4];
-         iswrite <= 1;
-      end else if( main_mem_write[5] ) begin
-         adr <= main_mem_write_adr[5];
-         write_dat <= main_mem_write_dat[5];
-         iswrite <= 1;
-      end else if( main_mem_write[6] ) begin
-         adr <= main_mem_write_adr[6];
-         write_dat <= main_mem_write_dat[6];
-         iswrite <= 1;
-      end else if( main_mem_write[7] ) begin
-         adr <= main_mem_write_adr[7];
-         write_dat <= main_mem_write_dat[7];
-         iswrite <= 1;
-      end else if( main_mem_read[0] ) begin
-         adr <= main_mem_write_adr[0];
-      end else if( main_mem_read[1] ) begin
-         adr <= main_mem_write_adr[1];
-      end else if( main_mem_read[2] ) begin
-         adr <= main_mem_write_adr[2];
-      end else if( main_mem_read[3] ) begin
-         adr <= main_mem_write_adr[3];
-      end else if( main_mem_read[4] ) begin
-         adr <= main_mem_write_adr[4];
-      end else if( main_mem_read[5] ) begin
-         adr <= main_mem_write_adr[5];
-      end else if( main_mem_read[6] ) begin
-         adr <= main_mem_write_adr[6];
-      end else if( main_mem_read[7] ) begin
-         adr <= main_mem_write_adr[7];
+      main_mem_read_ac <= 0;
+      if( main_mem_read_request_en ) begin
+         main_mem_read_ac <= 8'b0000_0001 << main_mem_read_request_q;
+      end
+   end
+
+   assign main_mem_ac = main_mem_write_ac | main_mem_read_ac;
+
+   always_comb begin
+      write_adr <= main_mem_write_adr[ main_mem_write_q ];
+      write_dat <= main_mem_write_dat[ main_mem_write_q ];
+   end
+
+   always_comb begin
+      read_adr <= main_mem_read_adr[ main_mem_read_q ];
+   end
+
+   always_comb begin
+      integer i;
+      for( i = 0; i < 8; i++ ) begin
+         lock_en_can[i] <= lock_en[i] & (!mutex[lock_adr[i]]);
       end
    end
 
    always_comb begin
-      lock_ac <= 0;      
-      if( unlock_en[0] ) begin
-         lock_ac <= 8'b00000001;
-      end else if( unlock_en[1] ) begin
-         lock_ac <= 8'b00000010;
-      end else if( unlock_en[2] ) begin
-         lock_ac <= 8'b00000100;
-      end else if( unlock_en[3] ) begin
-         lock_ac <= 8'b00001000;
-      end else if( unlock_en[4] ) begin
-         lock_ac <= 8'b00010000;
-      end else if( unlock_en[5] ) begin
-         lock_ac <= 8'b00100000;
-      end else if( unlock_en[6] ) begin
-         lock_ac <= 8'b01000000;
-      end else if( unlock_en[7] ) begin
-         lock_ac <= 8'b10000000;
-      end else if( lock_en[0] & (!mutex[lock_adr[0]]) ) begin
-         lock_ac <= 8'b00000001;
-      end else if( lock_en[1] & (!mutex[lock_adr[1]]) ) begin
-         lock_ac <= 8'b00000010;
-      end else if( lock_en[2] & (!mutex[lock_adr[2]]) ) begin
-         lock_ac <= 8'b00000100;
-      end else if( lock_en[3] & (!mutex[lock_adr[3]]) ) begin
-         lock_ac <= 8'b00001000;
-      end else if( lock_en[4] & (!mutex[lock_adr[4]]) ) begin
-         lock_ac <= 8'b00010000;
-      end else if( lock_en[5] & (!mutex[lock_adr[5]]) ) begin
-         lock_ac <= 8'b00100000;
-      end else if( lock_en[6] & (!mutex[lock_adr[6]]) ) begin
-         lock_ac <= 8'b01000000;
-      end else if( lock_en[7] & (!mutex[lock_adr[7]]) ) begin
-         lock_ac <= 8'b10000000;
+      enlock_ac <= 0;
+      if( enlock_do ) begin
+         enlock_ac <= 8'b0000_0001 << enlock_q;
       end
    end
+
+   always_comb begin
+      unlock_ac <= 0;
+      if( unlock_do ) begin
+         unlock_ac <= 8'b0000_0001 << unlock_q;
+      end
+   end
+
+   assign lock_ac = enlock_ac | unlock_ac;
 
    always_ff @( posedge clk ) begin
       if( reset ) begin
          mutex <= 0;
-      end else if( unlock_en[0] ) begin
-         mutex[ lock_adr[0] ] <= 0;
-      end else if( unlock_en[1] ) begin
-         mutex[ lock_adr[1] ] <= 0;
-      end else if( unlock_en[2] ) begin
-         mutex[ lock_adr[2] ] <= 0;
-      end else if( unlock_en[3] ) begin
-         mutex[ lock_adr[3] ] <= 0;
-      end else if( unlock_en[4] ) begin
-         mutex[ lock_adr[4] ] <= 0;
-      end else if( unlock_en[5] ) begin
-         mutex[ lock_adr[5] ] <= 0;
-      end else if( unlock_en[6] ) begin
-         mutex[ lock_adr[6] ] <= 0;
-      end else if( unlock_en[7] ) begin
-         mutex[ lock_adr[7] ] <= 0;
-      end else if( lock_en[0] & (!mutex[lock_adr[0]]) ) begin
-         mutex[ lock_adr[0] ] <= 1;
-      end else if( lock_en[1] & (!mutex[lock_adr[1]]) ) begin
-         mutex[ lock_adr[1] ] <= 1;
-      end else if( lock_en[2] & (!mutex[lock_adr[2]]) ) begin
-         mutex[ lock_adr[2] ] <= 1;
-      end else if( lock_en[3] & (!mutex[lock_adr[3]]) ) begin
-         mutex[ lock_adr[3] ] <= 1;
-      end else if( lock_en[4] & (!mutex[lock_adr[4]]) ) begin
-         mutex[ lock_adr[4] ] <= 1;
-      end else if( lock_en[5] & (!mutex[lock_adr[5]]) ) begin
-         mutex[ lock_adr[5] ] <= 1;
-      end else if( lock_en[6] & (!mutex[lock_adr[6]]) ) begin
-         mutex[ lock_adr[6] ] <= 1;
-      end else if( lock_en[7] & (!mutex[lock_adr[7]]) ) begin
-         mutex[ lock_adr[7] ] <= 1;
+      end else begin
+         if( enlock_do ) begin
+            mutex[ lock_adr[enlock_q] ] <= 1;
+         end
+         if( unlock_do ) begin
+            mutex[ lock_adr[unlock_q] ] <= 0;
+         end
       end
    end
    
+   always_ff @(negedge clk) begin
+      if( reset ) begin
+         cnt <= 0;
+      end else if( cnt == 7 ) begin
+         cnt <= 0;
+      end else begin
+         cnt <= cnt + 1;
+      end
+   end
+
    logic [15:0] RAM[65535:0];
 
    initial
      $readmemb( "dmem.bin", RAM );
    
-   assign main_mem_dat = RAM[adr];
+   assign main_mem_dat = RAM[read_adr];
 
    always_ff @(negedge clk)
-     if( iswrite ) RAM[adr] = write_dat;
+     if( iswrite ) RAM[write_adr] = write_dat;
+   
 endmodule
  
