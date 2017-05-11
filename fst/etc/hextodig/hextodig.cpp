be4bd2e2603895c074f8b7091e586b7ed35bb624
joisino <joisinoo@gmail.com>
2017-05-11 15:48:59 +0900

    add hextodig

---
+++ fst/etc/hextodig/hextodig.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+      
+#define FOR(i,a,b) for( ll i = (a); i < (ll)(b); i++ )
+#define REP(i,n) FOR(i,0,n)
+#define YYS(x,arr) for(auto& x:arr)
+#define ALL(x) (x).begin(),(x).end()
+#define SORT(x) sort( (x).begin(),(x).end() )
+#define REVERSE(x) reverse( (x).begin(),(x).end() )
+#define UNIQUE(x) (x).erase( unique( ALL( (x) ) ) , (x).end() )
+#define PW(x) (1LL<<(x))
+#define SZ(x) ((ll)(x).size())
+#define SHOW(x) cout << #x << " = " << x << endl
+#define SHOWA(x,n) for( int yui = 0; yui < n; yui++ ){ cout << x[yui] << " "; } cout << endl
+
+#define pb emplace_back
+#define fi first
+#define se second
+
+using namespace std;
+
+typedef long double ld;
+typedef long long int ll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+typedef vector<ld> vd;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpl> gr;
+typedef vector<vl> ml;
+typedef vector<vd> md;
+typedef vector<vi> mi;
+     
+const ll INF = (ll)1e9 + 10;
+const ll INFLL = (ll)1e18 + 10;
+const ld EPS = 1e-12;
+const ll MOD = 1e9+7;
+     
+template<class T> T &chmin( T &a , const T &b ){ return a = min(a,b); }
+template<class T> T &chmax( T &a , const T &b ){ return a = max(a,b); }
+template<class T> inline T sq( T a ){ return a * a; }
+
+ll in(){ long long int x; scanf( "%lld" , &x ); return x; }
+char yuyushiki[1000010]; string stin(){ scanf( "%s" , yuyushiki ); return yuyushiki; }
+
+// head
+
+int hextodig( char c ){
+  if( '0' <= c && c <= '9' ){
+    return c - '0';
+  } else if( 'A' <= c && c <= 'F' ){
+    return c - 'A' + 10;
+  }
+  assert( false );
+}
+
+int main(){
+
+  string s;
+  while( cin >> s ){
+    if( s.size() != 15 ){
+      break;
+    }
+    s = s.substr( 9 , 4 );
+    int x = 0;
+    YYS( w , s ){
+      x = x * 16 + hextodig( w );
+    }
+    cout << x << endl;
+  }
+  
+  return 0;
+}
