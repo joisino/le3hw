dfe98454ac4a978de924eeccc9e39ddb4ba11e43
joisino <1029275675@ml.ist.i.kyoto-u.ac.jp>
2017-05-12 17:42:57 +0900

    fix out in asm

--- fst/assembler/assembler.cpp
+++ fst/assembler/assembler.cpp
@@ -136,19 +136,19 @@ std::string decode( std::string inst ){
   } else if( op == "OUT" ){
     res += "11";
 
-    std::string rs;
-    std::string d;
+    std::string rs, rd;
     stin >> rs;
-    stin >> d;
+    stin >> rd;
     
     assert( rs[0] == 'r' );
     res += d_to_b( rs.substr( 1 ) , 3 );
 
-    res += "000";
+    assert( rd[0] == 'r' );
+    res += d_to_b( rd.substr( 1 ) , 3 );
 
     res += "1101";
 
-    res += d_to_b( d , 4 );
+    res += "0000";
   } else if( op == "HLT" ){
     res = "1100000011110000";
   } else if( op == "LD" || op == "ST" ){
