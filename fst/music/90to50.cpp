237a3910c256b574db4e707f7d8c9f38b7ccd913
yumakmc <yuma1192yuma2960@yahoo.co.jp>
2017-05-26 10:22:11 +0900

    merge music

---
+++ fst/music/90to50.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+      
+#define FOR(i,a,b) for( ll i = (a); i < (ll)(b); i++ )
+#define REP(i,n) FOR(i,0,n)
+#define YYS(x,arr) for(auto& x:arr)
+#define ALL(x) (x).begin(),(x).end()
+#define SORT(x) sort( (x).begin(),(x).end() )
+#define REVERSE(x) reverse( (x).begin(),(x).end() )
+#define UNIQUE(x) (x).erase( unique( ALL( (x) ) ) , (x).end() )
+#define PW(x) (1LL<<(x))
+#define SZ(x) ((ll)(x).size())
+#define SHOW(x) cout << #x << " = " << x << endl
+#define SHOWA(x,n) for( int yui = 0; yui < n; yui++ ){ cout << x[yui] << " "; } cout << endl
+
+#define pb emplace_back
+#define fi first
+#define se second
+
+using namespace std;
+
+typedef long double ld;
+typedef long long int ll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+typedef vector<ld> vd;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpl> gr;
+typedef vector<vl> ml;
+typedef vector<vd> md;
+typedef vector<vi> mi;
+     
+const ll INF = (ll)1e9 + 10;
+const ll INFLL = (ll)1e18 + 10;
+const ld EPS = 1e-12;
+const ll MOD = 1e9+7;
+     
+template<class T> T &chmin( T &a , const T &b ){ return a = min(a,b); }
+template<class T> T &chmax( T &a , const T &b ){ return a = max(a,b); }
+template<class T> inline T sq( T a ){ return a * a; }
+
+ll in(){ long long int x; scanf( "%lld" , &x ); return x; }
+char yuyushiki[1000010]; string stin(){ scanf( "%s" , yuyushiki ); return yuyushiki; }
+
+// head
+
+const int N = 36;
+const int M = 512;
+
+int a[N];
+int b[N];
+
+int table[M];
+
+int main(){
+
+  ifstream ifs90( "90.dat" );
+  ifstream ifs50( "50.dat" );
+
+  REP( i , N ){
+    ifs90 >> a[i];
+  }
+  REP( i , N ){
+    ifs50 >> b[i];
+  }
+
+  REP( i , N ){
+    table[ a[i] ] = b[i];
+  }
+
+  ifs90.close();
+  ifs50.close();
+
+  int x;
+  while( cin >> x ){
+    if( x >= M || table[x] == 0 ){
+      cout << x << endl;
+    } else {
+      cout << table[x] << endl;
+    }
+  }
+
+  return 0;
+}
+
+
